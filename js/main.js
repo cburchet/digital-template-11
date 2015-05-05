413fa922859f8bad9971b35e82ae9bde997acea9
cburchet <cburchet@gmu.edu>
2015-05-05 11:07:57 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -54,6 +54,7 @@ window.onload = function() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
         background = game.add.tileSprite(0, 0, 800, 600, 'background');
+        background.scale.setTo(3);
         
         group1 = game.add.group();
         group1.enableBody = true;
