50d96e879a5914586f6a949e57a988409d79104a
cburchet <cburchet@gmu.edu>
2015-05-04 21:17:50 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,38 +15,76 @@ window.onload = function() {
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+    function preload() 
+    {
+        game.load.image( 'crosshair', 'assets/crosshair.png' );
+        game.load.image( 'target1', 'assets/target1.png' );
+        game.load.image( 'target2', 'assets/target2.png' );
+        game.load.image( 'target3', 'assets/target3.png' );
+        game.load.image('bullet', 'assets/bullet.png');
     }
     
-    var bouncy;
+    var crosshair;
+    var group1;
+    var target1;
+    var group2;
+    var target2;
+    var group3;
+    var target3;
     
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+    var bullet;
+    var fireRate = 100;
+    var nextFire = 0;
+    
+    function create() 
+    {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        crosshair = game.add.sprite(game.input.activePointer.x, game.input.activePointer.y, 'crosshair');
         
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+        group1 = game.add.group();
+        group1.enableBody = true;
+        group2 = game.add.group();
+        group2.enableBody = true;
+        group3 = game.add.group();
+        group3.enableBody = true;
+        
+        
+        game.time.events.loop(Phaser.Timer.SECOND * 2, sendTarget1, this);
+        game.time.events.loop(Phaser.Timer.SECOND * 4, sendTarget2, this);
+        game.time.events.loop(Phaser.Timer.SECOND * 7, sendTarget3, this);
     }
     
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+    function update() 
+    {
+        
+        crosshair.x = game.input.activePointer.x;
+        crosshair.y = game.input.activePointer.y; 
+
+    }
+    
+    function fire()
+    {
+    	if (game.time.now > nextFire)
+    	{
+    		nextFire = game.time.now + fireRate;
+	    	bullet = game.add.sprite(game.input.activePointer.x, game.input.activePointer.y, 'bullet');
+	    	bullet.lifespan = 50;
+    	}
+    }
+    
+    function sendTarget1()
+    {
+        
+    }
+    
+    function sendTarget2()
+    {
+        
+    }
+    
+    function sendTarget3()
+    {
+        
     }
 };
