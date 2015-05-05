6e9076c172012e28e9cc68fee5b73d2b5e9d128c
cburchet <cburchet@gmu.edu>
2015-05-04 21:18:54 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -58,8 +58,8 @@ window.onload = function() {
     function update() 
     {
         
-        crosshair.x = game.input.activePointer.x;
-        crosshair.y = game.input.activePointer.y; 
+        crosshair.x = game.input.activePointer.x - 64;
+        crosshair.y = game.input.activePointer.y - 64; 
 
     }
     
